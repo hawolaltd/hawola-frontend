71eb7e05a9e1a8d4cea8ea1f8f81d53a92cecabe
ibrahimbank <bankoleibrahim96@gmail.com>
2025-02-08 16:45:46 +0100

    homepage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,46 +4,78 @@ import Slider from "react-slick";
 const Hero = () => {
 
     const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        dots: false, infinite: true, speed: 500, slidesToShow: 1, slidesToScroll: 1,
     };
 
+    const proSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        vertical: true,
+        verticalSwiping: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+    return (<section className="grid grid-cols-1 lg:grid-cols-6 gap-4 py-4 max-w-screen-xl mx-auto ">
 
-    return (
-        <section className="grid grid-cols-1 lg:grid-cols-6 gap-4 py-4 max-w-screen-xl mx-auto ">
 
-
-                <Slider {...settings} className={'rounded-xl col-span-3 bg-[#b8f2ff] bg-custom-bg bg-cover bg-center'}>
-                    <div className="w-full p-6">
-                        <h1 className="text-5xl font-bold text-gray-800 mb-6 text-left tracking-wide leading-snug">360 Degree <br/> Virtual Reality</h1>
-                        <ul className="text-lg text-gray-600 mb-8">
-                            <li>Free Shipping. Secure Payment</li>
-                            <li>Contact us 24hrs a day</li>
-                            <li>Support gift service</li>
-                        </ul>
-                        <div className={'flex items-center gap-1'}>
-                            <button className="bg-headerBg text-white py-3 px-6 rounded-lg text-sm">Shop Now</button>
-                            <button className="text-headerBg py-3 px-6 underline text-sm">Learn more</button>
-
-                        </div>
-                    </div>
-
-                    <div className=" w-full p-6">
-                        <h1 className="text-5xl font-bold text-gray-800 mb-6 text-left tracking-wide leading-snug">Welcome to E-Shop</h1>
-                        <p className="text-lg text-gray-600 mb-8">Discover the best deals on your favorite products</p>
+            <Slider {...settings} className={'rounded-xl col-span-3 bg-[#b8f2ff] bg-custom-bg bg-cover bg-center'}>
+                <div className="w-full p-6">
+                    <h1 className="text-5xl font-bold text-gray-800 mb-6 text-left tracking-wide leading-snug">360
+                        Degree <br/> Virtual Reality</h1>
+                    <ul className="text-lg text-gray-600 mb-8">
+                        <li>Free Shipping. Secure Payment</li>
+                        <li>Contact us 24hrs a day</li>
+                        <li>Support gift service</li>
+                    </ul>
+                    <div className={'flex items-center gap-1'}>
                         <button className="bg-headerBg text-white py-3 px-6 rounded-lg text-sm">Shop Now</button>
-                    </div>
+                        <button className="text-headerBg py-3 px-6 underline text-sm">Learn more</button>
 
-                    <div className="w-full p-6">
-                        <h1 className="text-5xl font-bold text-gray-800 mb-6 text-left tracking-wide leading-snug">Welcome to E-Shop</h1>
-                        <p className="text-lg text-gray-600 mb-8">Discover the best deals on your favorite products</p>
-                        <button className="bg-headerBg text-white py-3 px-6 rounded-lg text-sm">Shop Now</button>
                     </div>
+                </div>
 
-                </Slider>
+                <div className=" w-full p-6">
+                    <h1 className="text-5xl font-bold text-gray-800 mb-6 text-left tracking-wide leading-snug">Welcome
+                        to E-Shop</h1>
+                    <p className="text-lg text-gray-600 mb-8">Discover the best deals on your favorite products</p>
+                    <button className="bg-headerBg text-white py-3 px-6 rounded-lg text-sm">Shop Now</button>
+                </div>
+
+                <div className="w-full p-6">
+                    <h1 className="text-5xl font-bold text-gray-800 mb-6 text-left tracking-wide leading-snug">Welcome
+                        to E-Shop</h1>
+                    <p className="text-lg text-gray-600 mb-8">Discover the best deals on your favorite products</p>
+                    <button className="bg-headerBg text-white py-3 px-6 rounded-lg text-sm">Shop Now</button>
+                </div>
+
+            </Slider>
 
             <div className={'flex flex-col gap-6 items-center max-w-screen-2xl col-span-6 lg:col-span-2 '}>
                 <div
@@ -71,15 +103,17 @@ const Hero = () => {
 
                 </div>
             </div>
-            <div  className="w-full lg:w-auto mx-auto p-2 rounded border-2 border-solid border-[#fe9636] flex flex-col gap-2 text-center">
-                {
-                    ["/imgs/page/homepage4/promotion1.png","/imgs/page/homepage4/promotion2.png","/imgs/page/homepage4/promotion3.png","/imgs/page/homepage4/promotion4.png","/imgs/page/homepage4/promotion5.png", "/imgs/page/homepage4/promotion6.png"].map((item, key)=> (
-                        <img key={key} src={item} alt={''} />
-                    ))
-                }
+            <div
+                className="w-full lg:w-full mx-auto p-2 rounded border-2 border-solid border-[#fe9636] flex flex-col gap-2 text-center h-full ">
+                <Slider {...proSettings} className={'w-full h-full flex flex-col border border-primary'}>
+
+                    {["/imgs/page/homepage4/promotion1.png", "/imgs/page/homepage4/promotion2.png", "/imgs/page/homepage4/promotion3.png", "/imgs/page/homepage4/promotion4.png", "/imgs/page/homepage4/promotion5.png", "/imgs/page/homepage4/promotion6.png"].map((item, key) => (
+                        <div key={key} className="w-full flex flex-col h-full border border-primary">
+                            <img src={item} alt={''}/>
+                        </div>))}
+                </Slider>
             </div>
-        </section>
-    );
+        </section>);
 }
 
 export default Hero;
