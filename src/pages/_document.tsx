eb020236bd325066cc606c12f9fa805f0af6f4f9
Oluwasegun Bello <bellosegunc@gmail.com>
2026-06-10 23:25:32 +0100

    PWA FIX

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,6 @@ export default function Document() {
         <link rel="icon" href="/fav.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="theme-color" content="#0E224D" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Hawola" />
         <link rel="apple-touch-icon" href="/fav.png" />
       </Head>
